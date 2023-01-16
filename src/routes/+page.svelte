6b8c18981efb660bd01a9757ed6b768e9c8f6f1e
1AndaroX1 <roma.chernukha.007@gmail.com>
2023-01-16 06:40:46 +0300

    добалена альфа карточка товара

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
 <section>
-    <div></div>
+	<div />
 </section>
 
-<section>
-
+<section class="flex flex-wrap flex-row flex-start container mx-auto mt-8 px-8">
+	<div class="flex flex-wrap flex-row items-stretch">
+		<div class="flex flex-initial bg-slate-400 flex-col m-3">
+			<div class="flex flex-1 min-h-min">
+				<a href="a">
+					<img
+						src="../picture/makita-df457dwe.jpg"
+						alt="none"
+						class="h-64 items-center rounded-sm"
+					/>
+				</a>
+			</div>
+			<div>какойто шуруповерт чето крутит вертит</div>
+		</div>
+	</div>
 </section>
