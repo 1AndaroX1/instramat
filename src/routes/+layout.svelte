028cd1eea53a690f403d48fb086f0a5cd175d043
1AndaroX1 <roma.chernukha.007@gmail.com>
2023-01-16 17:37:04 +0300

    сегодня пена сделал рандомные карточки

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -62,16 +62,28 @@
 	<div class="container mx-auto flex flex-row  font-semibold px-4">
 		<div class="flex flex-row container justify-between mx-auto gap-4 py-2 items-center">
 			<a href="/">
-				<img src="../picture/Logo.svg" alt="instramat.by" class="min-w-full w-24" />
+				<img src="../picture/Logo.svg" alt="instramat.by" class="min-w-full w-24 invert" />
 			</a>
 			<div class="flex  container justify-between mx-auto px-2 gap-4 py-2 items-center">
 				<p><a href="/">О нас</a></p>
 				<p><a href="/">Контакты</a></p>
-				<div class="flex">
-					<a href="/"
-						><img src="../picture/TelegramLogo.svg" alt="telegram" class="h-6 w-6 mx-4" /></a
+				<div class="flex gap-4">
+					<a href="/">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-6 w-6"
+							><path
+								d="M9.78 18.65l.28-4.23l7.68-6.92c.34-.31-.07-.46-.52-.19L7.74 13.3L3.64 12c-.88-.25-.89-.86.2-1.3l15.97-6.16c.73-.33 1.43.18 1.15 1.3l-2.72 12.81c-.19.91-.74 1.13-1.5.71L12.6 16.3l-1.99 1.93c-.23.23-.42.42-.83.42z"
+								fill="currentColor"
+							/></svg
+						></a
 					>
-					<a href="/"><img src="../picture/InstagramLogo.svg" alt="instagram" class="h-6 w-6" /></a>
+					<a href="/"
+						><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24 " class="h-6 w-6"
+							><path
+								fill="currentColor"
+								d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3Z"
+							/></svg
+						>
+					</a>
 				</div>
 			</div>
 		</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,65 @@
+<script>
+	import { faker } from '@faker-js/faker';
+	import { each } from 'svelte/internal';
+
+	const products = [];
+
+	function createRandomProducts() {
+		return {
+			photo: faker.image.technics(640, 480, true),
+			name: faker.commerce.productName(),
+			price: faker.finance.amount()
+		};
+	}
+
+	Array.from({ length: 8 }).forEach(() => {
+		products.push(createRandomProducts());
+	});
+</script>
+
 <section>
 	<div />
 </section>
 
 <section class="flex flex-wrap flex-row flex-start container mx-auto mt-8 px-8">
-	<div class="flex flex-wrap flex-row items-stretch">
-		<div class="flex flex-initial bg-slate-400 flex-col m-3">
+	<div class="grid grid-cols-4 gap-3 items-stretch mb-6 w-full">
+		<!-- карточка товара -->
+		{#each products as product}
+			<div class="flex flex-initia flex-col p-3 shadow-md gap-3 rounded-md">
+				<div class="flex flex-1 min-h-min">
+					<a href="a">
+						<img src={product.photo} alt="none" class="items-center rounded-sm w-full" />
+					</a>
+				</div>
+				<div class="text">{product.name}</div>
+				<div>{product.price} р. / сут</div>
+				<button
+					class="font-semibold bg-main text-zinc-50 rounded-full px-4 cursor-pointer py-2 flex self-start"
+					>В корзину</button
+				>
+			</div>
+		{/each}
+	</div>
+</section>
+
+<!-- <section class="flex flex-wrap flex-row flex-start container mx-auto mt-8 px-8">
+	<div class="flex flex-wrap flex-row gap-3 items-stretch mb-6">
+		<div class="flex flex-initia flex-col w-64 p-3 shadow-md gap-3 rounded-md">
 			<div class="flex flex-1 min-h-min">
 				<a href="a">
 					<img
 						src="../picture/makita-df457dwe.jpg"
 						alt="none"
-						class="h-64 items-center rounded-sm"
+						class="items-center rounded-sm w-full"
 					/>
 				</a>
 			</div>
-			<div>какойто шуруповерт чето крутит вертит</div>
+			<div class="text">Шуруповерт-дрель Makita-df457dwe</div>
+			<div>25 р. / сут</div>
+			<button
+				class="font-semibold bg-main text-zinc-50 rounded-full px-4 cursor-pointer py-2 flex self-start"
+				>В корзину</button
+			>
 		</div>
 	</div>
-</section>
+</section> -->
